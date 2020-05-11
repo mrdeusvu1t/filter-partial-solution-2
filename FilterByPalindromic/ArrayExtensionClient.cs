976b4f34cd004f4cdbe5314120ad65d66b179f52
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-05-11 18:21:45 +0000

    add-client-file

---
+++ FilterByPalindromic/ArrayExtensionClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FilterByPalindromic
+{
+    public static partial class ArrayExtension
+    {
+        //TODO: Add code here if necessary.
+        
+        //TODO: Add parameters to the method if necessary.
+        static partial void AddAccordingToPredicate()
+        {
+            throw new NotImplementedException("");
+        }
+    }
+}
