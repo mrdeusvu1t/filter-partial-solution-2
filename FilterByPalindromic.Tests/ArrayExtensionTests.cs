ee893e70d36b4c40d8a14d3c2012a2e5f5ae57f8
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-05-12 08:48:52 +0000

    Update ArrayExtensionTests.cs

--- FilterByPalindromic.Tests/ArrayExtensionTests.cs
+++ FilterByPalindromic.Tests/ArrayExtensionTests.cs
@@ -5,6 +5,12 @@ namespace FilterByPalindromic.Tests
     [TestFixture]
     public class ArrayExtensionTests
     {
-        //TODO: Add test methods.
+        [TestCase(new[] {1, 2, 3, 4, 5, 6, 7, 8, 9}, ExpectedResult = new[] {1, 2, 3, 4, 5, 6, 7, 8, 9})]
+        [TestCase(new[] {717, 828, 45, 58, 881, 11711, 252}, ExpectedResult = new[] {717, 828, 11711, 252})]
+        [TestCase(new[] {17, 82, 45, 58, 881, 117, 25}, ExpectedResult = new int[] { })]
+        public int[] FilterByPalindromicTests(int[] source)
+            => source.FilterByPredicate();
+        
+        //TODO: Add tests for Exception cases here.
     }
 }
