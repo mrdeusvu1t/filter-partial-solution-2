cfa2155808dd589cb588b4776b788974f5d6ed7e
Aliaksandr Nikulin <aliaksandr_nikulin@epam.com>
2020-07-20 08:41:57 +0000

    Update ArrayExtensionTests.cs

--- FilterByPalindromic.Tests/ArrayExtensionTests.cs
+++ FilterByPalindromic.Tests/ArrayExtensionTests.cs
@@ -14,7 +14,7 @@ namespace FilterByPalindromic.Tests
             ExpectedResult = new[] {0, 1, 123321, 1111111})]
         [TestCase(new[] {1111111112, 987654, -24, 1234654321, 32, 1005}, ExpectedResult = new int[] { })]
         [TestCase(new[] {-27, 987656789, 7557, int.MaxValue, 7556, 7243, 7243427, int.MinValue},
-            ExpectedResult = new[] {7557, 7243427})]
+            ExpectedResult = new[] {987656789, 7557, 7243427})]
         [TestCase(new[] {111, 111, 111, 11111111}, ExpectedResult = new[] {111, 111, 111, 11111111})]
         [TestCase(new[] {-1, 0, 111, -11, -1}, ExpectedResult = new[] {0, 111})]
         [TestCase(new[] {0, 1, 2, 3, 4}, ExpectedResult = new[] {0, 1, 2, 3, 4})]
